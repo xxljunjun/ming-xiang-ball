44cf72693977c9ee8caeae8b6a63c99e8eee94f3
junjun <793117268@qq.com>
2021-10-24 10:48:53 +0800

    初始化工程项目

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import App from './App'
-import store from './store'
-import Vuex from 'vuex'
-Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-  ...App,
-  store
+  ...App
 })
 app.$mount()
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,28 +1,49 @@
 <template>
-  <view class="home">
-    <TabBar />
-  </view>
+	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
 </template>
 
 <script>
-import TabBar from '@/components/tabBar.vue'
-export default {
-  data() {
-    return {
-      token: '',
-    }
-  },
-  mounted() {},
-  onLoad() {},
-  methods: {},
-  components: {
-    TabBar,
-  },
-}
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
 </script>
 
-<style lang="scss" scoped>
-.home {
-  color: #000;
-}
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin: 200rpx auto 50rpx auto;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
 </style>
